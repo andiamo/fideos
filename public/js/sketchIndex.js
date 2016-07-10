5e3c69245461b74b09a1e30408e13acff459a1ca
codeanticode <andres.colubri@gmail.com>
2016-07-10 02:09:44 -0400

    nube de palabras

--- public/js/sketchIndex.js
+++ public/js/sketchIndex.js
@@ -1,8 +1,17 @@
 var logo;
 
-//esto es para probar nada mas
-var angle = 90;
-var speed = 0.02;
+var noiseScale = 0.01;
+
+var orbit;
+var w, h;
+
+var palabras = ['dibujo', 'en vivo', 'colectivo', 'en línea', 'para tod@s', 'en diálogo', 'en tiempo real', 'animado'];
+var sizes =    [      24,        18,          24,         18,           24,           18,               24,        18];
+var angles =   [       0,         0,           0,          0,            0,            0,                0,         0];
+var speeds =   [       0,         0,           0,          0,            0,            0,                0,         0];
+
+// var angle = 90;
+// var speed = 0.01;
 
 function setup() {
 
@@ -10,26 +19,56 @@ function setup() {
     var canvasHeight = $("#modalIndex .logo").height() + 0.2 * 0.8 * windowHeight;
     var canvas = createCanvas(0.8*windowWidth, canvasHeight);
     canvas.parent('canvas');
+    w = width;
+    h = height;
+
 
     //De esta manera orbita alrededor del logo
     orbit = $("#modalIndex .logo").width() * 0.75;
 
     //esto es para probar nada mas
-    stroke(255);
-    noFill();
+    // stroke(255);
+    // noFill();
+    noStroke();
+    fill(0, 102, 0);
+
+    for (var i = 0; i < palabras.length; i++) {
+        angles[i] = random(TWO_PI);
+        speeds[i] = (random() > 0.5 ? +1 : -1) * random(0.003, 0.006);
+    }
+
     textAlign(CENTER);
+    textFont("Lucida Sans Typewriter");
 }
 
 function draw() {
-    // Acá se puede poner la lógica para que se dibuje la nube
-    // hice una simple prueba
     clear();
-    translate(width/2,height/2);
-    text("prueba", sin(angle) * orbit * 1.5, cos(angle) * orbit);
-    text("prueba2", sin(-angle) * orbit * 1.5, cos(-angle) * orbit);
-    angle+=speed;
+    translate(w/2, h/2);
 
-    if ( angle >= 360 ) {
-        angle = 0;
+    for (var i = 0; i < palabras.length; i++) {
+        var pala = palabras[i];
+        var angle = angles[i];
+        var size = sizes[i];
+        var speed = speeds[i];
+        textSize(size);
+
+        var x = sin(angle) * orbit * 1.5;
+        var y = cos(angle) * orbit;
+        var r = noise(x * noiseScale, y * noiseScale);
+        x = sin(angle) * orbit * 1.5 + (70 * r);
+        y = cos(angle) * orbit + (50 * r);
+        text(pala, x, y);    
+        angle += speed;
+        if (abs(angle) >= TWO_PI) {
+            angle = 0;
+        }
+        angles[i] = angle;
     }
+}
+
+function windowResized() {
+    var canvasHeight = $("#modalIndex .logo").height() + 0.2 * 0.8 * windowHeight;
+    resizeCanvas(0.8*windowWidth, canvasHeight);
+    w = width;
+    h = height;  
 }
