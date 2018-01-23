73a15503badbd5039a4f0596c51b9f486d946289
glozano <gazzon@gmail.com>
2018-01-23 12:20:21 -0300

    - Typo and small fix

--- public/js/trazos/board/share.js
+++ public/js/trazos/board/share.js
@@ -2,7 +2,7 @@ document.getElementById('shareBtn').onclick = function() {
     FB.ui({
         method: 'share',
         display: 'popup',
-        quote: "¡Unete a mi tablero y creemos un dibujo juntos!",
+        quote: "¡Únete a mi tablero y creemos un dibujo juntos!",
         href: window.location.href,
     }, function(response){});
 }
