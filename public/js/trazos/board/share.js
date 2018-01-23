0c929249ede03d2f5ef527da4d5ed41743d713bb
glozano <gazzon@gmail.com>
2018-01-23 12:16:26 -0300

    - Added logo with bg - Changed FB share image - Changed FB quote

--- public/js/trazos/board/share.js
+++ public/js/trazos/board/share.js
@@ -2,6 +2,7 @@ document.getElementById('shareBtn').onclick = function() {
     FB.ui({
         method: 'share',
         display: 'popup',
+        quote: "¡Unete a mi tablero y creemos un dibujo juntos!",
         href: window.location.href,
     }, function(response){});
 }
